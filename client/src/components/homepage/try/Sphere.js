aac0f9af42b85bb711aeafc593c0996a2682f358
dingyuanzhang <zhangd7@myumanitoba.ca>
2024-03-29 11:10:36 -0500

    add style

--- client/src/components/homepage/try/Sphere.js
+++ client/src/components/homepage/try/Sphere.js
@@ -22,6 +22,7 @@ const Sphere = () => {
             'Authorization': 'Bearer ' + token,
           },
         });
+        
         const data = await res.json();
         const users = data.listUsers.map(user => ({
           id: user._id,
